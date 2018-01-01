c5ae3f45f6855476e6e04f60946fc120ef498071
qiankeqin <951669478@qq.com>
2018-01-01 18:25:23 +0800

    引入quartz框架，定时发送邮件

--- bos-parent/bos-dao/src/main/java/com/itheima/bos/dao/IWorkbillDao.java
+++ bos-parent/bos-dao/src/main/java/com/itheima/bos/dao/IWorkbillDao.java
@@ -1,8 +1,12 @@
 package com.itheima.bos.dao;
 
+import java.util.List;
+
 import com.itheima.bos.dao.base.IBaseDao;
 import com.itheima.bos.domain.Workbill;
 
 public interface IWorkbillDao extends IBaseDao<Workbill>{
+
+	List<Workbill> findNewWorkbills();
 
 }
--- bos-parent/bos-dao/src/main/java/com/itheima/bos/dao/impl/WorkbillDao.java
+++ bos-parent/bos-dao/src/main/java/com/itheima/bos/dao/impl/WorkbillDao.java
@@ -1,5 +1,7 @@
 package com.itheima.bos.dao.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.itheima.bos.dao.IWorkbillDao;
@@ -8,5 +10,15 @@ import com.itheima.bos.domain.Workbill;
 
 @Repository
 public class WorkbillDao extends BaseDaoImpl<Workbill> implements IWorkbillDao{
+
+	/**
+	 * 获取新单数据
+	 */
+	@Override
+	public List<Workbill> findNewWorkbills() {
+		String hql = "From Workbill w where w.type='新单'";
+		List<Workbill> list = (List<Workbill>) this.getHibernateTemplate().find(hql);
+		return list;
+	}
 
 }
